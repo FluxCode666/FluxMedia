1eb35646b08641b6d6e7913da63310e714c45ac5
MoYeRanQianZhi <MoYeRanQianZhi@gmail.com>
2026-04-24 15:28:04 +0800

    feat: add image generation backend with platform API + user custom API config

---
+++ src/features/image-generation/index.ts
@@ -0,0 +1,3 @@
+export type { GenerateImageParams, GenerateImageResult, ApiConfig } from "./types";
+export { generateImageAction } from "./actions";
+export { getUserApiConfig, generateImage, getEffectiveConfig } from "./service";
--- src/features/settings/actions/index.ts
+++ src/features/settings/actions/index.ts
@@ -1,3 +1,4 @@
 // Settings feature - action exports
 export { deleteAccountAction } from "./delete-account";
 export { updateProfileAction } from "./update-profile";
+export { getApiConfig, saveApiConfig, deleteApiConfig, toggleApiConfig } from "./api-config";
